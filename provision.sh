bf9b7e2e00d8eb5bfbae4389ea6b92bf16ccd86d
Jonathon Brouse <jbrouse19@gmail.com>
2016-02-12 21:54:56 -0500

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,7 +4,10 @@ INSTALL_PATH="$(pwd)"
 
 apt-get update
 apt-get -y upgrade
-apt-get -y install puppet screen vim
+apt-get -y install puppet screen vim curl
 
 puppet module install garethr-docker
 puppet module install KyleAnderson-consul
+
+curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x usr/local/bin/docker-compose
