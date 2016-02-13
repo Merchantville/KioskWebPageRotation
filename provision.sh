960936cad08dbede5e4d868a2eef676850eb1a92
Jonathon Brouse <jbrouse@zonoff.com>
2016-02-13 14:25:01 +0000

    Removed puppet from install and added Docker

--- provision.sh
+++ provision.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-INSTALL_PATH="$(pwd)"
+function install_docker {
+  apt-get install apt-transport-https ca-certificates
+  apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+  echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+  apt-get update
+  apt-cache policy docker-engine
+  apt-get install docker-engine
+  service docker start
+  
+  curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  chmod +x usr/local/bin/docker-compose
+}
 
 apt-get update
-apt-get -y upgrade
-apt-get -y install puppet screen vim curl
+apt-get -y install screen vim curl
 
-puppet module install garethr-docker
-puppet module install KyleAnderson-consul
-
-curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x usr/local/bin/docker-compose
+install_docker
