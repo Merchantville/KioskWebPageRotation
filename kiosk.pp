aacd3b6484093a7dbb9dc126f8d4324399688276
Jonathon Brouse <jbrouse@zonoff.com>
2016-02-14 20:00:48 +0000

    Now adding a user in kiosk.pp

--- kiosk.pp
+++ kiosk.pp
@@ -1,4 +1,18 @@
 node 'compute-kiosk-01' {
+  
+  user  { "webops": 
+    ensure  => present,
+    uid     => '1001',
+  }
+
+  package { 'screen': ensure => 'installed'}
+  package { 'curl': ensure => 'installed'}
+  package { 'lxde-core': ensure => 'installed'}
+
+  class { 'vim':
+    opt_bg_shading  => light,
+  }
+  
   class { 'docker':
     docker_users => 'webops',
   }
