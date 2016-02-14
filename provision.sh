7510eda6a7356a3b4c224726937d10ca4de17fd1
Jonathon Brouse <jbrouse19@gmail.com>
2016-02-13 20:40:52 -0500

    add webops to docker group

--- provision.sh
+++ provision.sh
@@ -11,6 +11,8 @@ function install_docker {
   
   curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x usr/local/bin/docker-compose
+  
+  usermod -a -G docker webops
 }
 
 apt-get update
