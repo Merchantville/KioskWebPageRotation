3b1f2f9bf7f7db761b430f14b84d9e9e6a794c06
Jonathon Brouse <jbrouse@zonoff.com>
2016-03-04 06:26:29 +0000

    cleaned up docker-compose, start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-docker-compose -f /home/jbrouse/projects_git/kiosk/docker-compose.yml up -d
-sleep 5
+#startx
+xhost +
+docker-compose -f /opt/kiosk/docker-compose.yml pull
+docker-compose -f /opt/kiosk/docker-compose.yml up -d
+sleep 15
 docker exec kiosk xdotool key F11
