5c84626bd963fe9101c1a3079e33067b1d781312
Jonathon Brouse <jbrouse19@gmail.com>
2016-05-31 20:11:29 -0400

    moved update.sh script

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,8 @@
 
 IMAGE="merchantville/kiosk"
 CONTAINER_ID=$(docker ps | grep $IMAGE | awk '{print $1}')
+COMPOSE_FILE="/home/jbrouse/projects_git/kiosk/docker-compose.yml"
+DOCKER_COMPOSE="docker-compose -f $COMPOSE_FILE"
 
 docker pull $IMAGE
 
@@ -10,8 +12,12 @@ RUNNING=`docker inspect --format "{{.Image}}" $CONTAINER_ID`
 
 if [ "$RUNNING" != "$LATEST" ]; then
   echo "Upgrading $IMAGE"
-  stop kiosk
-  start kiosk
+  #stop kiosk
+  #start kiosk
+  $DOCKER_COMPOSE kill
+  $DOCKER_COMPOSE rm -f
+  $DOCKER_COMPOSE pull
+  $DOCKER_COMPOSE up -d
 else
   echo "$IMAGE is up to date."
 fi
