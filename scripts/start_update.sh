ca296f77f190bfc330915548b75bca301db200b9
Jonathon Brouse <jbrouse19@gmail.com>
2016-05-31 23:29:01 -0400

    updated start_update script:

--- scripts/start_update.sh
+++ scripts/start_update.sh
@@ -2,16 +2,20 @@
 
 IMAGE="merchantville/kiosk"
 CONTAINER_ID=$(docker ps | grep $IMAGE | awk '{print $1}')
-COMPOSE_FILE="/home/jbrouse/projects_git/kiosk/docker-compose.yml"
+COMPOSE_FILE="/home/$(whoami)/projects_git/kiosk/docker-compose.yml"
 DOCKER_COMPOSE="docker-compose -f $COMPOSE_FILE"
 
 docker pull $IMAGE
 
-LATEST=`docker inspect --format "{{.Id}}" $IMAGE`
-RUNNING=`docker inspect --format "{{.Image}}" $CONTAINER_ID`
+LATEST=$(docker inspect --format "{{.Id}}" $IMAGE)
+RUNNING=$(docker inspect --format "{{.Image}}" $CONTAINER_ID)
+STATUS=$(docker ps | grep kiosk)
 
-if [ "$RUNNING" != "$LATEST" ]; then
-  echo "Upgrading $IMAGE"
+
+if [ "$RUNNING" != "$LATEST" ] || [ -z "$STATUS" ]; then
+  xhost +local:
+
+  echo "Upgrading/starting $IMAGE"
 
   #TODO Create upstart scripts
   #stop kiosk
@@ -19,6 +23,9 @@ if [ "$RUNNING" != "$LATEST" ]; then
 
   $DOCKER_COMPOSE kill && $DOCKER_COMPOSE rm -f
   $DOCKER_COMPOSE pull && $DOCKER_COMPOSE up -d
+
+  echo "Waiting for the container to come up..."
+  sleep 25
 
   docker exec kiosk xdotool key F11
 else
